aeac49344a25b5ac03802445894b61c625fe5c15
zhangqi <2334521489@qq.com>
2023-05-27 09:38:05 +0800

    第一天：数据类型和变量

---
+++ day1/变量-1/变量-1.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// 变量就如同一个事物的属性，你可以去定义
+func main() {
+	//1.变量的声明
+	var age int
+	//2.变量的赋值
+	age = 18
+	//3.变量的使用
+	fmt.Println("age = ", age)
+
+	//声明和赋值可以合成一句：
+	var age2 int = 19
+	fmt.Println("age2 = ", age2)
+
+	// var age int = 20;
+	// fmt.Println("age = ",age);
+	/*变量的重复定义会报错：
+	  # command-line-arguments
+	  .\变量-1.go:16:6: age redeclared in this block
+	                  previous declaration at .\变量-1.go:6:6
+	*/
+
+	//不可以在赋值的时候给与不匹配的类型
+	//var num int = 12.56
+	//fmt.Println("num = ", num)
+	fmt.Println(ApplicationAndLocality())
+
+}
+
+func ApplicationAndLocality() {
+	fmt.Println()
+}
